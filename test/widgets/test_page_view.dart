a1e766bfe78abde14dc4a497cf1c6266f0131e17
Jerinji2016 <jerinji2016@gmail.com>
2023-09-01 00:37:45 +0530

    sample test widget

---
+++ test/widgets/test_page_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_view/infinite_scroll_view.dart';
+
+class TestPageView extends StatefulWidget {
+  final InfinitePageController? controller;
+
+  const TestPageView({
+    Key? key,
+    this.controller,
+  }) : super(key: key);
+
+  @override
+  State<TestPageView> createState() => _TestPageViewState();
+}
+
+class _TestPageViewState extends State<TestPageView> {
+  late final InfinitePageController controller = widget.controller ?? InfinitePageController();
+
+  late int currentIndex = controller.page.toInt();
+
+  void _onPageChanged(int index) => currentIndex = index;
+
+  @override
+  Widget build(BuildContext context) {
+    return InfinitePageView(
+      controller: widget.controller,
+      onPageChanged: _onPageChanged,
+      itemBuilder: (context, index) {
+        return Text(
+          "Page $index",
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        );
+      },
+    );
+  }
+}
